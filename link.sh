b6579ee8c688b30dbdcdf2bb70875292f19fd9b9
0x1f8f <0x1f8f@skiff.com>
2023-12-10 12:19:58 +0530

    update

---
+++ link.sh
@@ -0,0 +1,1 @@
+ln $PWD/* $HOME/.config/ -s
--- nvim/lua/plugins/base.lua
+++ nvim/lua/plugins/base.lua
@@ -8,5 +8,6 @@ return {
 
     {   'nvim-telescope/telescope.nvim', tag = '0.1.5',
         dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+    },
+    "numToStr/FTerm.nvim",
 }
