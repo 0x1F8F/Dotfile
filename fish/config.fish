7dd35b8b71f4da03fb53bf1a850240a7c8a89766
0x1F8F <86254906+0x1F8F@users.noreply.github.com>
2024-07-25 03:15:05 +0530

    initial commit

--- fish/config.fish
+++ fish/config.fish
@@ -7,11 +7,13 @@ function fish_greeting
     # greet
 end
 
+eval $(ssh-agent -c) > /dev/null
+ssh-add /Users/RootCl/.ssh/gh 2> /dev/null
 alias ls lsd
 alias ip "ip -c"
 alias vi nvim
 alias vim nvim
-alias code " vscodium --enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform=wayland --unity-launch"
+# alias code " vscodium --enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform=wayland --unity-launch"
 source (/usr/bin/starship init fish --print-full-init | psub)
 
 
