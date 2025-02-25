cc5f43199ccc05c7570f1ce5067cfc9239475b01
0x1F8F <86254906+0x1F8F@users.noreply.github.com>
2025-02-25 16:48:02 +0530

    fixed function:envsource:

--- fish/functions/envsource.fish
+++ fish/functions/envsource.fish
@@ -1,8 +1,17 @@
 function envsource
-	echo "Exposing variables"
-  for line in (cat $argv | grep -v '^#')
-    set item (string split -m 1 '=' $line)
-    set -gx $item[1] $item[2]
-    echo "$item[1]"
+	if test $argv[1]
+		set file $argv[1]
+	else
+		set file .env
+	end
+
+	if ! test -e $file
+		echo 'envsource [file | default:.env]'
+		return 1
+	end
+
+	for i in (cat $file | grep -v '^#')
+       set item (string split "=" $i)
+       set -gx $item[1] $item[2] 2> /dev/null || printf '[\033[32mwarn\033[0m] \033[31m]'$item[1]'\033[0m\n'
   end
 end
